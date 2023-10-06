f2e451b8db30b33e0e755accfc997c2cdb573609
happy gikundiro <gikundirohappy@gmail.com>
2023-10-07 01:56:21 +0200

    added customer section

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Stories from "./components/stories";
 import Subscription from "./components/subscription";
 function App() {
   return (
-    <div className=" space-y-10 font-abc sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300">
+    <div className=" space-y-10 font-abc ">
       {/* sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300 */}
       <Navbar />
       <Content />
